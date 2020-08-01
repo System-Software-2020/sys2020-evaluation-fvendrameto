86b1a34906a77860b751e0b46a1b2f8f3dbca4b8
fvendrameto <fernando@sigalei.com.br>
2020-08-01 18:07:46 -0300

    fix ex2.c

--- ex2.c
+++ ex2.c
@@ -2,6 +2,6 @@
 #include <sys/syscall.h>
 
 extern ssize_t write(int file_descriptor, const void *buffer, size_t buffer_size) {
-    syscall(SYS_write, file_descriptor, buffer, buffer_size - 1);
+    syscall(SYS_write, file_descriptor, buffer, buffer_size);
     return 0;
 }
